939d5b2b35e17022b18cd56608215ba95f20bc49
Rich-85 <127021654+Rich-85@users.noreply.github.com>
2024-06-11 22:16:45 +1200

    Adding Profile and Tasks pages Also adding .css code for tasks.html page Adding links for newly created pages

--- cleanSweep.py
+++ cleanSweep.py
@@ -10,5 +10,13 @@ def main():
 def contact():
     return render_template("contact.html")
 
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+@app.route('/tasks')
+def tasks():
+    return render_template("tasks.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
