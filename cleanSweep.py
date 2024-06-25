b30f35adb1af8afc48211f11c7e803fdbcf7be48
MJBytes <270232978@yoobeestudent.ac.nz>
2024-06-25 12:23:24 +1200

    Started with a model, looking into creating a class for user profiles

--- cleanSweep.py
+++ cleanSweep.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+#custom base class for SQLAlchemy models
+class Base(DeclarativeBase):
+  pass
 
 #create SQL config extension
-db = SQLAlchemy()
+db = SQLAlchemy(model_class=Base)
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 #initialize the app with the extension
 db.init_app(app)
